d4ccb089d3ffba20de58abbc6a0f014e1ee25a0d
Bolortuya Jargalsaikhan <bolortuyajargalsaikhan@Bolortuyas-MacBook-Air.local>
2025-08-14 12:02:40 +0800

    profile

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import userRouter from "./router/user.router";
 import geminiRouter from "./router/gemini.router";
-
+import profileRouter from "./router/profile.router";
 const app = express();
 
 const PORT = 4001;
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use("/users", userRouter);
 app.use("/gemini", geminiRouter);
-// app.use("profile",profileRouter); 
+app.use("/profile",profileRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
