3908ec771f840619b877e3c8e8ae7a83620ae131
25LP9262 25LP9262 <25LP9262@admins-iMac.local>
2025-08-18 12:29:38 +0800

    count,accuracy

--- src/controller/Gemini/get-reading-count.controller.ts
+++ src/controller/Gemini/get-reading-count.controller.ts
@@ -4,12 +4,63 @@ import { prisma } from "../../utils/prisma";
 export const getReadingCount = async (req: Request, res: Response) => {
   try {
     const { profileId } = req.params;
-    const count = await prisma.reading.count({
-      where: { profileId: Number(profileId) },
+
+    const { startOfWeek, endOfWeek } = getStartAndEnd();
+
+    const readings = await prisma.reading.findMany({
+      where: {
+        profileId: Number(profileId),
+        NOT: {
+          endTime: null,
+        },
+        endTime: {
+          gte: startOfWeek,
+          lte: endOfWeek,
+        },
+      },
     });
-    res.json({ count });
+
+    const totalDurationMs = readings.reduce((sum, reading) => {
+      if (reading.startTime && reading.endTime) {
+        return (
+          sum +
+          (new Date(reading.endTime).getTime() -
+            new Date(reading.startTime).getTime())
+        );
+      }
+      return sum;
+    }, 0);
+
+    const totalDurationMinutes = totalDurationMs / 1000 / 60;
+
+    const averageAccuracy = readings.reduce(
+      (acc, curr) => acc + Number(curr.accuracy),
+      0
+    );
+
+    res.json({
+      count: readings?.length,
+      averageDuration: totalDurationMinutes.toFixed(2),
+      averageAccuracy,
+    });
   } catch (err) {
     console.error("Reading тоо авах алдаа:", err);
     res.status(500).json({ message: "Reading тоо авах үед алдаа гарлаа" });
   }
+};
+
+const getStartAndEnd = () => {
+  const today = new Date();
+  const startOfWeek = new Date(today);
+  startOfWeek.setDate(startOfWeek.getDate() - startOfWeek.getDay());
+  startOfWeek.setHours(0, 0, 0, 0);
+
+  const endOfWeek = new Date(startOfWeek);
+  endOfWeek.setDate(startOfWeek.getDate() + 6);
+  endOfWeek.setHours(23, 59, 59, 999);
+
+  return {
+    startOfWeek,
+    endOfWeek,
+  };
 };
--- src/router/gemini.router.ts
+++ src/router/gemini.router.ts
@@ -1,9 +1,8 @@
 import express from "express";
-import { getWords } from "../controller/Gemini/get-generate-word.controller";
-import { finishReading } from "../controller/Gemini/finish-reading.controller";
-import { checkWords } from "../controller/Gemini/post-check-word.controller";
-import { getReadingCount } from "../controller/Gemini/get-reading-count.controller";
-
+import { getReadingCount } from "../controller/gemini/get-reading-count.controller";
+import { checkWords } from "../controller/gemini/post-check-word.controller";
+import { finishReading } from "../controller/gemini/finish-reading.controller";
+import { getWords } from "../controller/gemini/get-generate-word.controller";
 
 const geminiRouter = express.Router();
 
