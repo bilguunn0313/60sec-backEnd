9f43ecc974bec1c43635b99e5ae9dd0865668e46
25LP9262 25LP9262 <25LP9262@admins-iMac.local>
2025-09-02 12:09:45 +0800

    level fix

--- src/router/gemini.router.ts
+++ src/router/gemini.router.ts
@@ -6,6 +6,8 @@ import { getReadingCount } from "../controller/Gemini/get-reading-count.controll
 import { getSentence } from "../controller/Gemini/get-generate-word.controller";
 import { finishReading } from "../controller/Gemini/finish-reading.controller";
 import { checkWords } from "../controller/Gemini/post-check-word.controller";
+import { getLevelTwo } from "../controller/Gemini/get-level-2.controller";
+import { getLevelThree } from "../controller/Gemini/get-level-3.controller";
 
 const geminiRouter = express.Router();
 
@@ -17,5 +19,9 @@ geminiRouter.put("/finish/:readingId", finishReading);
 geminiRouter.post("/check-words", checkWords);
 
 geminiRouter.get("/stats/:profileId", getReadingCount);
+
+geminiRouter.post("/levelTwo/:profileId", getLevelTwo);
+
+geminiRouter.post("/levelThree/:profileId", getLevelThree);
 
 export default geminiRouter;
